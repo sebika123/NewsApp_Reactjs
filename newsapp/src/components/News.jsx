9a1f5c9a8ed260aa08a464ed4388e331e695728d
Sebika Nepal <sebikanepal4@gmail.com>
2023-07-15 23:10:43 +0545

    fetching data using async

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -1,6 +1,23 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 export default class News extends Component {
+
+  
+    constructor(){
+      super();
+      this.state={
+        articles:this.articles,
+        loading:false
+      }
+    }
+    async componentDidMount(){
+      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd1a7ad3a5b74680a5c659a6eeabf9b3"
+      let data=await fetch(url);
+      let parsedData=await data.json()
+      console.log(parsedData);
+      this.setState({articles:parsedData.articles})
+    }
+
   render() {
     return (
      <><div className="container my-3">
@@ -8,16 +25,16 @@ export default class News extends Component {
       <h2>
         News
       </h2>
+      
       <div className="row">
-        <div className="col-md-4">
-        <NewsItem title="myTitle" description="mydesc" imageUrl=""/>
-        </div>
-        <div className="col-md-4">
-        <NewsItem title="myTitle" description="mydesc"/>
-        </div>
-        <div className="col-md-4">
-        <NewsItem title="myTitle" description="mydesc"/>
-        </div>
+      {this.state.articles.map((element)=>{
+return  <div className="col-md-4" key={element.url}>
+<NewsItem  title={element?element.title.slice(0,30):""} description={element.description ? element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+</div>
+
+
+      })}
+       
       
       </div>
 
--- newsapp/src/components/NewsItem.jsx
+++ newsapp/src/components/NewsItem.jsx
@@ -2,22 +2,20 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
 
-  constructor(){
-    super();
-  }
+
   
   render() {
 
-    let {title,description,imageUrl}=this.props;
+    let {title,description,imageUrl,newsUrl}=this.props;
     return (
      <>
      
      <div className="card" style={{width: "18rem"}}>
-  <img src="{imageUrl}" className="card-img-top" alt="..."/>
+  <img src={imageUrl } className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/" className="btn btn-primary">Go somewhere</a>
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
   </div>
 </div>
      
