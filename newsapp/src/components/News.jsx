eca774297d8ecba871255c8c372af787a93f7a42
Sebika Nepal <sebikanepal4@gmail.com>
2023-07-16 22:58:41 +0545

    api data fetch done

--- newsapp/src/components/News.jsx
+++ newsapp/src/components/News.jsx
@@ -6,36 +6,74 @@ export default class News extends Component {
     constructor(){
       super();
       this.state={
-        articles:this.articles,
-        loading:false
+        articles:[],
+        loading:false,
+        page:1,
+
       }
     }
     async componentDidMount(){
-      let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd1a7ad3a5b74680a5c659a6eeabf9b3"
+      let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd1a7ad3a5b74680a5c659a6eeabf9b3&pageSize=${this.props.pageSize}`
       let data=await fetch(url);
       let parsedData=await data.json()
       console.log(parsedData);
-      this.setState({articles:parsedData.articles})
+      this.setState({articles:parsedData.articles,totalResults:parsedData.totalResults})
     }
+
+    handlePreClick= async()=>{
+      
+
+      
+      let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd1a7ad3a5b74680a5c659a6eeabf9b3&page=${this.state.page+1}&pageSIze=${this.props.pageSize}`;
+      let data=await fetch(url);
+      let parsedData=await data.json()
+      console.log(parsedData);
+
+     
+      this.setState({
+      page:this.state.page-1,
+      articles:parsedData.articles
+      })
+    
+    }
+
+    handleNexClick=async()=>{
+if(this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize)) {
+
+      }
+      else{
+      let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=bd1a7ad3a5b74680a5c659a6eeabf9b3&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+      let data=await fetch(url);
+      let parsedData=await data.json()
+      console.log(parsedData);
+
+     
+      this.setState({
+      page:this.state.page+1,
+      articles:parsedData.articles
+})
+       } }
 
   render() {
     return (
      <><div className="container my-3">
 
-      <h2>
-        News
-      </h2>
+      <h1 className="text-center">News</h1>
       
       <div className="row">
       {this.state.articles.map((element)=>{
 return  <div className="col-md-4" key={element.url}>
-<NewsItem  title={element?element.title.slice(0,30):""} description={element.description ? element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+<NewsItem  title={element.title?element.title.slice(0,30):""} description={element.description ? element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
 </div>
 
 
       })}
        
       
+      </div>
+      <div className="container d-flex justify-content-between">
+      <button  disabled={this.state.page<=1} type="button" className="btn btn-primary" onClick={this.handlePreClick}>&larr; Previous</button>
+      <button disabled={ (this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)) } type="button" className="btn btn-primary" onClick={this.handleNexClick}>Next &rarr;</button>
       </div>
 
      </div>
--- newsapp/src/components/NewsItem.jsx
+++ newsapp/src/components/NewsItem.jsx
@@ -11,11 +11,11 @@ export default class NewsItem extends Component {
      <>
      
      <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl } className="card-img-top" alt="..."/>
+  <img src={!imageUrl?"https://www.syracuse.com/resizer/uarCgNK6_HNaB-6u1DkbYawEii8=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/NZIIYWVEQVERZH4LQKQ7SIRAEI.jpg":imageUrl } className="card-img-top" alt="..."/>
   <div className="card-body">
     <h5 className="card-title">{title}...</h5>
     <p className="card-text">{description}...</p>
-    <a href={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
+    <a href={newsUrl}  rel="noreferrer"  target="_blank" className="btn btn-primary">Read More</a>
   </div>
 </div>
      
