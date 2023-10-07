0a35ea4d0917df43489e3ec7e0cea48a37d2b418
sebika nepal <72345247+sebika123@users.noreply.github.com>
2023-10-07 23:00:06 +0545

    routing working

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -2,19 +2,25 @@
 import './App.css';
 import React, { Component } from 'react'
 import Navbar from'./components/Navbar';
-
-
-
 import News from'./components/News';
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Link
+} from "react-router-dom";
+
+
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Router>
         <Navbar/>
         {/* prop bata news content ma country pass garne */}
-        <News  pageSize={5} country="in" />
+        <News  pageSize={5} country="in" category="general"/>
 
-        
+        </Router>
 
       </div>
     )
